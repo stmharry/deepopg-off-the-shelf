d5174a96d35cea60470c4366032362a1b6576557
arlen <a870128rlen@gmail.com>
2023-09-14 06:46:34 +0000

    eval with acc but no plot roc curve and remove exclude

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import numpy as np
 from pathlib import Path
-from sklearn.metrics import roc_curve, auc
+
+import pandas as pd
 from absl import app, flags, logging
+from sklearn.metrics import auc, roc_curve
 
 # common arguments
 flags.DEFINE_string("result_dir", None, "Result directory.")
@@ -16,12 +16,17 @@ flags.DEFINE_bool("do_acc", False, "Whether to do # implant acc.")
 FLAGS = flags.FLAGS
 
 
-def ROC(
-    df_golden: pd.DataFrame,
-    df_result: pd.DataFrame
-) -> None:
-    
-    list_finding = ["MISSING", "IMPLANT", "ROOT_REMNANTS", "CARIES", "ENDO", "CROWN_BRIDGE", "PERIAPICAL_RADIOLUCENT", "FILLING"]
+def ROC(df_golden: pd.DataFrame, df_result: pd.DataFrame) -> None:
+    list_finding = [
+        "MISSING",
+        "IMPLANT",
+        "ROOT_REMNANTS",
+        "CARIES",
+        "ENDO",
+        "CROWN_BRIDGE",
+        "PERIAPICAL_RADIOLUCENT",
+        "FILLING",
+    ]
     file_list = pd.unique(df_golden["file_name"]).tolist()
 
     for finding_name in list_finding:
@@ -41,10 +46,18 @@ def ROC(
                 ]
             )
 
-            df_full_tooth["golden"] = df_full_tooth["fdi"].isin(file_golden["fdi"]).astype(int)
-            df_full_tooth = pd.merge(df_full_tooth, file_result[["fdi", "score"]], left_on="fdi", right_on="fdi", how="left")
+            df_full_tooth["golden"] = (
+                df_full_tooth["fdi"].isin(file_golden["fdi"]).astype(int)
+            )
+            df_full_tooth = pd.merge(
+                df_full_tooth,
+                file_result[["fdi", "score"]],
+                left_on="fdi",
+                right_on="fdi",
+                how="left",
+            )
             df_full_tooth["score"].fillna(0, inplace=True)
-            
+
             df_auc = pd.concat([df_auc, df_full_tooth], ignore_index=True)
 
         golden = df_auc["golden"]
@@ -56,23 +69,38 @@ def ROC(
         print(roc_auc)
 
 
-    
+def ACC(
+    df_golden: pd.DataFrame,
+    df_result: pd.DataFrame
+) -> None:
+    df_count = df_result[df_result["finding"] == "IMPLANT"].groupby("file_name").size().reset_index(name='num_implant')
+    unique_names = df_result['file_name'].unique()
+    missing_names = set(unique_names) - set(df_count['file_name'])
+    missing_data = pd.DataFrame({'file_name': list(missing_names), 'num_implant': [0] * len(missing_names)})
+    df_count = pd.concat([df_count, missing_data], ignore_index=True)
 
-# def ACC(
-        
-# ) -> None:
+    df_golden["finding"] = df_golden["finding"] == "IMPLANT"
+    df_golden = df_golden.groupby("file_name")["finding"].sum().reset_index()
+
+    # names_to_remove = df_implant.loc[df_implant["exclude"] == True, "file_name"]
+    # df_golden = df_golden[~df_golden["file_name"].isin(names_to_remove)]
+    # df_count = df_count[~df_count["file_name"].isin(names_to_remove)]
+
+    merged_df = df_golden.merge(df_count, on="file_name", how="left")
+
+    acc = sum(merged_df["finding"] == merged_df["num_implant"]) / len(merged_df)
+    print(acc)
 
 
 def main(_):
     logging.set_verbosity(logging.INFO)
     df_golden = pd.read_csv(Path(FLAGS.golden_dir, FLAGS.golden_csv_name))
     df_result = pd.read_csv(Path(FLAGS.result_dir, FLAGS.output_csv_name))
-    
 
     if FLAGS.do_roc:
         ROC(df_golden=df_golden, df_result=df_result)
-    # if FLAGS.do_acc:
-    #     ACC()
+    if FLAGS.do_acc:
+        ACC(df_golden=df_golden, df_result=df_result)
 
 
 if __name__ == "__main__":
