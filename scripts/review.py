e80369f8099d442277468886b0e110dd615d4040
arlen <a870128rlen@gmail.com>
2023-11-06 16:13:56 +0000

    correct the path

--- scripts/review.py
+++ scripts/review.py
@@ -19,17 +19,18 @@ def us_fdi(x: int):
     fdi = grid * 10 + index
     return fdi
 
+# raw data -> golden.csv
 def Golden(
         dataset_name: str = "ntuh"
 ) -> pd.DataFrame:
     if dataset_name == "ntuh":
-        sheet = pd.read_csv("/home/arlen/deepopg-eval/review/(WIP) NTUH Summary Golden Label - Per-study.csv")
+        sheet = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/(WIP) NTUH Summary Golden Label - Per-study.csv")
         with open('/mnt/md0/data/PANO/data/raw/NTUH/ntuh-opg-12.json', 'r') as json_file:
             data = json.load(json_file)
     images = data["images"]
     lookup_file = {item["id"]: item["file_name"].split(".")[0] for item in images}
 
-    row_results = []
+    row_results: list = []
     for i, id in enumerate(sheet["No."]):
         file_name = lookup_file.get(id, None)
         if file_name is None:
@@ -60,33 +61,36 @@ def Golden(
 
     # create csv
     df_golden: pd.DataFrame = pd.DataFrame(row_results)
+    # df_result.to_csv("/mnt/hdd/PANO.arlen/golden/ntuh_golden.csv", index=False)
 
     return df_golden
 
 
 def dentist_review(
         dentist_name: str="A",
-) -> pd.DataFrame:
-    pano_finding_1_df = pd.read_csv("/home/arlen/deepopg-eval/review/Pano Finding - 1 (Responses) - Form Responses 1.csv")
-    pano_finding_2_df = pd.read_csv("/home/arlen/deepopg-eval/review/Pano Finding - 2 (Responses) - Form responses 1.csv")
-    pano_finding_3_df = pd.read_csv("/home/arlen/deepopg-eval/review/Pano Finding - 3 (Responses) - Form responses 1.csv")
-    pano_finding_4_df = pd.read_csv("/home/arlen/deepopg-eval/review/Pano Finding - 4 (Responses) - Form responses 1.csv")
+) -> None:
+    pano_finding_1_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/Pano Finding - 1 (Responses) - Form Responses 1.csv")
+    pano_finding_2_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/Pano Finding - 2 (Responses) - Form responses 1.csv")
+    pano_finding_3_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/Pano Finding - 3 (Responses) - Form responses 1.csv")
+    pano_finding_4_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/Pano Finding - 4 (Responses) - Form responses 1.csv")
 
-    pano_image_1_df = pd.read_csv("/home/arlen/deepopg-eval/review/1.csv", header=None)
-    pano_image_2_df = pd.read_csv("/home/arlen/deepopg-eval/review/2.csv", header=None)
-    pano_image_3_df = pd.read_csv("/home/arlen/deepopg-eval/review/3.csv", header=None)
-    pano_image_4_df = pd.read_csv("/home/arlen/deepopg-eval/review/4.csv", header=None)
+    pano_image_1_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/1.csv", header=None)
+    pano_image_2_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/2.csv", header=None)
+    pano_image_3_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/3.csv", header=None)
+    pano_image_4_df = pd.read_csv("/mnt/hdd/PANO.arlen/data/raw_data/4.csv", header=None)
 
     pano_finding = [[pano_finding_1_df, pano_image_1_df], [pano_finding_2_df, pano_image_2_df], [pano_finding_3_df, pano_image_3_df], [pano_finding_4_df, pano_image_4_df]]
-
     dentist_list = []
+    # total 120 images
+    image_list = pd.concat([image[1] for df, image in pano_finding], ignore_index=True).values.tolist()
+    dentist_image_list = [filename.replace('.png', '') for filename in image_list]
+    # only 117 images was reviewed by dentists
     for df, image in pano_finding:
-        for tooth_name in range(1,31):
+        for tooth_name in range(1,32):
             mask = df.columns.str.startswith("{}-".format(tooth_name))
             your_number = df['Your Number']
             result = pd.concat([your_number, df.loc[:, mask]], axis=1)
             filtered_result = result[result['Your Number'] == dentist_name]
-
             for i in filtered_result.columns[1:]:
                 if filtered_result[i].isin(list_finding_dentist).any():
                     dentist_list.append(
@@ -96,53 +100,83 @@ def dentist_review(
                             "finding": dict_finding_dentist[filtered_result[i].values[0]]
                         }
                     )
-
-        df_result: pd.DataFrame = pd.DataFrame(dentist_list)
-
-    return df_result
+    df_result: pd.DataFrame = pd.DataFrame(dentist_list)
+    return df_result, dentist_image_list
 
 
 def ROC_curve(
         df_golden: pd.DataFrame,
         df_result: pd.DataFrame,
-        df_dentist: pd.DataFrame
-) -> None:
-    # filter the ROC curve in the 120 images of dentist reviews
-    file_list = pd.unique(df_dentist["file_name"]).tolist()
-    fpr_finding = []
-    tpr_finding = []
-    auc_finding = []
+        list_dentist: list,
+) -> list:
+    list_dentist = list_dentist
+    fpr_finding: list = []
+    tpr_finding: list = []
+    auc_finding: list = []
     for finding_name in list_finding_PANO:
-        df_golden_finding = df_golden[df_golden["finding"] == finding_name]
-        df_result_finding = df_result[df_result["finding"] == finding_name]
-
         df_auc = pd.DataFrame()
-        for i in file_list:
-            file_golden = df_golden_finding[df_golden_finding["file_name"] == i]
-            file_result = df_result_finding[df_result_finding["file_name"] == i]
+        # missing tooth
+        if finding_name == "MISSING" or finding_name == "IMPLANT":
+            df_golden_finding = df_golden[df_golden["finding"] == finding_name]
+            df_result_finding = df_result[df_result["finding"] == finding_name]
+            for i in list_dentist:
+                file_golden = df_golden_finding[df_golden_finding["file_name"] == i]
+                file_result = df_result_finding[df_result_finding["file_name"] == i]
 
-            df_full_tooth: pd.DataFrame = pd.DataFrame(
-                [
-                    {"fdi": int(f"{quadrant}{tooth}")}
-                    for quadrant in range(1, 5)
-                    for tooth in range(1, 9)
-                ]
-            )
+                df_full_tooth: pd.DataFrame = pd.DataFrame(
+                    [
+                        {"fdi": int(f"{quadrant}{tooth}")}
+                        for quadrant in range(1, 5)
+                        for tooth in range(1, 9)
+                    ]
+                )
 
-            df_full_tooth["golden"] = (
-                df_full_tooth["fdi"].isin(file_golden["fdi"]).astype(int)
-            )
+                df_full_tooth["golden"] = (
+                    df_full_tooth["fdi"].isin(file_golden["fdi"]).astype(int)
+                )
+                file_result.set_index('fdi', inplace=True)
+                df_full_tooth.set_index('fdi', inplace=True)
+                df_full_tooth['score'] = file_result['score']
+                df_full_tooth.reset_index(inplace=True)
+                df_full_tooth["score"].fillna(0, inplace=True)
 
-            df_full_tooth = pd.merge(
-                df_full_tooth,
-                file_result[["fdi", "score"]],
-                left_on="fdi",
-                right_on="fdi",
-                how="left",
-            )
-            df_full_tooth["score"].fillna(0, inplace=True)
+                df_auc = pd.concat([df_auc, df_full_tooth], ignore_index=True)
+        # no missing tooth
+        else:
+            df_golden_missing = df_golden[df_golden["finding"] == "MISSING"]
+            df_golden_finding = df_golden[df_golden["finding"] == finding_name]
+            df_result_finding = df_result[df_result["finding"] == finding_name]
 
-            df_auc = pd.concat([df_auc, df_full_tooth], ignore_index=True)
+            for i in list_dentist:
+                file_missing = df_golden_missing[df_golden_missing["file_name"] == i]
+                file_golden = df_golden_finding[df_golden_finding["file_name"] == i]
+                file_result = df_result_finding[df_result_finding["file_name"] == i]
+
+                df_full_tooth: pd.DataFrame = pd.DataFrame(
+                    [
+                        {"fdi": int(f"{quadrant}{tooth}")}
+                        for quadrant in range(1, 5)
+                        for tooth in range(1, 9)
+                    ]
+                )
+
+                df_full_tooth["golden"] = (
+                    df_full_tooth["fdi"].isin(file_golden["fdi"]).astype(int)
+                )
+
+                file_result.set_index('fdi', inplace=True)
+                df_full_tooth.set_index('fdi', inplace=True)
+                df_full_tooth['score'] = file_result['score']
+                df_full_tooth.reset_index(inplace=True)
+                df_full_tooth["score"].fillna(0, inplace=True)
+                
+                df_full_tooth["missing"] = (
+                    df_full_tooth["fdi"].isin(file_missing["fdi"]).astype(int)
+                )
+                df_full_tooth = df_full_tooth[df_full_tooth["missing"] == 0].drop(columns=['missing'])
+
+                # save the dataframe with golden and score without missing
+                df_auc = pd.concat([df_auc, df_full_tooth], ignore_index=True)
 
         golden = df_auc["golden"]
         score = df_auc["score"]
@@ -153,17 +187,17 @@ def ROC_curve(
         fpr_finding.append(fpr)
         tpr_finding.append(tpr)
         auc_finding.append(roc_auc)
-        
-
+            
     return fpr_finding, tpr_finding, auc_finding
+
     
 
 def confus_dentist(
         df_golden: pd.DataFrame,
         df_dentist: pd.DataFrame
 ) -> list:
-    TPR = []
-    FPR = []
+    TPR: list = []
+    FPR: list = []
     file_list = pd.unique(df_dentist["file_name"]).tolist()
     for finding_name in list_finding_PANO:
         df_golden_finding = df_golden[df_golden["finding"] == finding_name]
@@ -200,16 +234,16 @@ def confus_dentist(
 
 golden_dataframe = Golden(dataset_name="ntuh")
 dentist_name = ["A", "C", "D", "E"]
-TPR_alldentist = []
-FPR_alldentist = []
+TPR_alldentist: list = []
+FPR_alldentist: list = []
 for dentist in dentist_name:
-    dentist_dataframe = dentist_review(dentist_name=dentist)
+    dentist_dataframe, dentist_image_list = dentist_review(dentist_name=dentist)
     dentist_dataframe.to_csv("tmp/dentist_{}.csv".format(dentist), index=False)
     TPR, FPR = confus_dentist(golden_dataframe, dentist_dataframe)
     TPR_alldentist.append(TPR)
     FPR_alldentist.append(FPR)
 
-fpr, tpr, roc_auc = ROC_curve(golden_dataframe, df_result, dentist_dataframe)
+fpr, tpr, roc_auc = ROC_curve(golden_dataframe, df_result, dentist_image_list)
 
 for id, finding_name in enumerate(list_finding_PANO):
     plt.figure(figsize=(8, 6))
