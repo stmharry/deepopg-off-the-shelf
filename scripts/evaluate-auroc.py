3610bc16e15e146e766088999c40840454211a8f
Harry Hsu <h@stmharry.io>
2024-02-19 13:50:01 +0000

    refactor: move plotting to a separate function

--- scripts/evaluate-auroc.py
+++ scripts/evaluate-auroc.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import sklearn.metrics
 from absl import app, flags, logging
+from matplotlib.figure import Figure
 
 from app.instance_detection.types import (
     EVALUATE_WHEN_MISSING_FINDINGS,
@@ -64,6 +65,163 @@ def process_per_tooth(df: pd.DataFrame) -> pd.DataFrame:
     df = df.loc[df["finding"].isin(findings)]
 
     return df
+
+
+def plot_roc_curve(
+    df: pd.DataFrame,
+    human_tags: list[str],
+) -> Figure:
+    num_columns: int = FLAGS.plots_per_row
+    num_rows: int = math.ceil(len(Category) / num_columns)
+    fig, axes = plt.subplots(
+        nrows=num_rows,
+        ncols=num_columns,
+        sharey=True,
+        figsize=(FLAGS.plot_size * num_columns, FLAGS.plot_size * num_rows),
+    )
+
+    for num, finding in enumerate(Category):
+        metadata = CATEGORY_METADATA[finding]
+        df_finding = df.loc[df["finding"].eq(finding.value)].copy()
+
+        P = df_finding["label"].eq(1).sum()
+        N = df_finding["label"].eq(0).sum()
+
+        fpr, tpr, _ = sklearn.metrics.roc_curve(
+            y_true=df_finding["label"],
+            y_score=df_finding["score"],
+            drop_intermediate=False,
+        )
+
+        tpr_std_err: np.ndarray = np.sqrt(tpr * (1 - tpr) / P)
+        tpr_ci_lower: np.ndarray = np.maximum(0, tpr - 1.96 * tpr_std_err)
+        tpr_ci_upper: np.ndarray = np.minimum(1, tpr + 1.96 * tpr_std_err)
+
+        # https://www.ncss.com/wp-content/themes/ncss/pdf/Procedures/PASS/Confidence_Intervals_for_the_Area_Under_an_ROC_Curve.pdf
+
+        roc_auc: float = sklearn.metrics.roc_auc_score(  # type: ignore
+            y_true=df_finding["label"],
+            y_score=df_finding["score"],
+        )
+
+        Q1: float = roc_auc / (2 - roc_auc)
+        Q2: float = 2 * roc_auc**2 / (1 + roc_auc)
+
+        auc_std_err: float = np.sqrt(
+            (
+                roc_auc * (1 - roc_auc)
+                + (P - 1) * (Q1 - roc_auc**2)
+                + (N - 1) * (Q2 - roc_auc**2)
+            )
+            / (P * N)
+        )
+        roc_auc_ci_lower: float = np.maximum(0, roc_auc - 1.96 * auc_std_err)
+        roc_auc_ci_upper: float = np.minimum(1, roc_auc + 1.96 * auc_std_err)
+
+        report_by_tag: dict[str, dict] = {}
+        for tag in human_tags:
+            report: dict = sklearn.metrics.classification_report(  # type: ignore
+                y_true=df_finding["label"],
+                y_pred=df_finding[f"score_human_{tag}"],
+                output_dict=True,
+            )
+
+            report_by_tag[tag] = report
+
+        # plotting
+
+        ax = axes.flatten()[num]
+
+        ax.grid(
+            visible=True,
+            which="major",
+            linestyle="--",
+            linewidth=0.5,
+        )
+        ax.fill_between(
+            fpr,
+            tpr_ci_lower,
+            tpr_ci_upper,
+            color=metadata["color"],
+            alpha=0.2,
+            linewidth=0.0,
+        )
+        ax.plot(
+            [0, 1],
+            [0, 1],
+            color="k",
+            linestyle="--",
+            linewidth=0.75,
+        )
+        ax.plot(
+            fpr,
+            tpr,
+            color=metadata["color"],
+            linewidth=0.75,
+            label="AI System",
+        )
+
+        for tag, report in report_by_tag.items():
+            human_metadata = HUMAN_METADATA[tag]
+
+            ax.plot(
+                1 - report["0"]["recall"],
+                report["1"]["recall"],
+                color=human_metadata["color"],
+                marker=human_metadata["marker"],
+                markersize=2,
+                linewidth=0.0,
+                label=human_metadata["title"],
+            )
+
+        ax.legend(
+            loc="lower right",
+            fontsize="x-small",
+        )
+
+        xticks: np.ndarray = np.linspace(0, 1, 6)
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(["{:,.0%}".format(v) for v in xticks])
+
+        yticks: np.ndarray = np.linspace(0, 1, 6)
+        ax.set_yticks(yticks)
+        ax.set_yticklabels(["{:,.0%}".format(v) for v in yticks])
+
+        ax.set_xlabel("1 - Specificity")
+        if num % num_columns == 0:
+            ax.set_ylabel("Senstivity")
+
+        ax.set_title(f"{metadata['title']} (AUC = {roc_auc:.1%})")
+
+        logging.info(
+            f"Finding {finding.value}\n"
+            f"  - Sample Count: {len(df_finding)}\n"
+            f"  - Positive Count: {P}\n"
+            f"  - Negative Count: {N}\n"
+            f"  - AUROC: {roc_auc:.1%} ({roc_auc_ci_lower:.1%}, {roc_auc_ci_upper:.1%})"
+        )
+
+    fig.tight_layout()
+
+    return fig
+
+
+def extract_false_negatives(df: pd.DataFrame) -> pd.DataFrame:
+    _df_fns: list[pd.DataFrame] = []
+    for finding in Category:
+        df_finding = df.loc[df["finding"].eq(finding.value)]
+
+        _df_fns.append(
+            df_finding.loc[(df_finding.label == 1.0) & (df_finding.score == 0.0)].drop(
+                columns=["label", "score"]
+            )
+        )
+
+    df_fn: pd.DataFrame = pd.concat(_df_fns, axis=0, ignore_index=True).sort_values(
+        ["file_name", "fdi", "finding"]
+    )
+
+    return df_fn
 
 
 def main(_):
@@ -144,148 +302,11 @@ def main(_):
 
     #
 
-    num_columns: int = FLAGS.plots_per_row
-    num_rows: int = math.ceil(len(Category) / num_columns)
-    fig, axes = plt.subplots(
-        nrows=num_rows,
-        ncols=num_columns,
-        sharey=True,
-        figsize=(FLAGS.plot_size * num_columns, FLAGS.plot_size * num_rows),
-    )
-
-    _df_fns: list[pd.DataFrame] = []
-    for num, finding in enumerate(Category):
-        metadata = CATEGORY_METADATA[finding]
-        df_finding = df.loc[df["finding"].eq(finding.value)]
-
-        P = df_finding["label"].eq(1).sum()
-        N = df_finding["label"].eq(0).sum()
-
-        fpr, tpr, _ = sklearn.metrics.roc_curve(
-            y_true=df_finding["label"],
-            y_score=df_finding["score"],
-            drop_intermediate=False,
-        )
-        tpr_std_err: np.ndarray = np.sqrt(tpr * (1 - tpr) / P)
-        tpr_ci_lower: np.ndarray = np.maximum(0, tpr - 1.96 * tpr_std_err)
-        tpr_ci_upper: np.ndarray = np.minimum(1, tpr + 1.96 * tpr_std_err)
-
-        # https://www.ncss.com/wp-content/themes/ncss/pdf/Procedures/PASS/Confidence_Intervals_for_the_Area_Under_an_ROC_Curve.pdf
-
-        roc_auc: float = sklearn.metrics.roc_auc_score(  # type: ignore
-            y_true=df_finding["label"],
-            y_score=df_finding["score"],
-        )
-
-        Q1: float = roc_auc / (2 - roc_auc)
-        Q2: float = 2 * roc_auc**2 / (1 + roc_auc)
-
-        auc_std_err: float = np.sqrt(
-            (
-                roc_auc * (1 - roc_auc)
-                + (P - 1) * (Q1 - roc_auc**2)
-                + (N - 1) * (Q2 - roc_auc**2)
-            )
-            / (P * N)
-        )
-        roc_auc_ci_lower: float = np.maximum(0, roc_auc - 1.96 * auc_std_err)
-        roc_auc_ci_upper: float = np.minimum(1, roc_auc + 1.96 * auc_std_err)
-
-        report_by_tag: dict[str, dict] = {}
-        for tag, df_human in df_human_by_tag.items():
-            report: dict = sklearn.metrics.classification_report(  # type: ignore
-                y_true=df_finding["label"],
-                y_pred=df_finding[f"score_human_{tag}"],
-                output_dict=True,
-            )
-
-            report_by_tag[tag] = report
-
-        # plotting
-
-        ax = axes.flatten()[num]
-
-        ax.grid(
-            visible=True,
-            which="major",
-            linestyle="--",
-            linewidth=0.5,
-        )
-        ax.fill_between(
-            fpr,
-            tpr_ci_lower,
-            tpr_ci_upper,
-            color=metadata["color"],
-            alpha=0.2,
-            linewidth=0.0,
-        )
-        ax.plot(
-            [0, 1],
-            [0, 1],
-            color="k",
-            linestyle="--",
-            linewidth=0.75,
-        )
-        ax.plot(
-            fpr,
-            tpr,
-            color=metadata["color"],
-            linewidth=0.75,
-            label="AI System",
-        )
-
-        for tag, report in report_by_tag.items():
-            human_metadata = HUMAN_METADATA[tag]
-
-            ax.plot(
-                1 - report["0"]["recall"],
-                report["1"]["recall"],
-                color=human_metadata["color"],
-                marker=human_metadata["marker"],
-                markersize=2,
-                linewidth=0.0,
-                label=human_metadata["title"],
-            )
-
-        ax.legend(
-            loc="lower right",
-            fontsize="x-small",
-        )
-
-        xticks: np.ndarray = np.linspace(0, 1, 6)
-        ax.set_xticks(xticks)
-        ax.set_xticklabels(["{:,.0%}".format(v) for v in xticks])
-
-        yticks: np.ndarray = np.linspace(0, 1, 6)
-        ax.set_yticks(yticks)
-        ax.set_yticklabels(["{:,.0%}".format(v) for v in yticks])
-
-        ax.set_xlabel("1 - Specificity")
-        if num % num_columns == 0:
-            ax.set_ylabel("Senstivity")
-
-        ax.set_title(f"{metadata['title']} (AUC = {roc_auc:.1%})")
-
-        _df_fns.append(
-            df_finding.loc[(df_finding.label == 1.0) & (df_finding.score == 0.0)]
-            .drop(columns=["label", "score"])
-            .sort_values(["file_name", "fdi"])
-        )
-
-        logging.info(
-            f"Finding {finding.value}\n"
-            f"  - Sample Count: {len(df_finding)}\n"
-            f"  - Positive Count: {P}\n"
-            f"  - Negative Count: {N}\n"
-            f"  - AUROC: {roc_auc:.1%} ({roc_auc_ci_lower:.1%}, {roc_auc_ci_upper:.1%})"
-        )
-
-    fig.tight_layout()
+    fig: Figure
+    fig = plot_roc_curve(df, human_tags=list(HUMAN_METADATA.keys()))
     fig.savefig(Path(evaluation_dir, f"roc-curve.pdf"))
 
-    df_fn: pd.DataFrame = pd.concat(_df_fns, axis=0, ignore_index=True).sort_values(
-        ["file_name", "fdi", "finding"]
-    )
+    df_fn: pd.DataFrame = extract_false_negatives(df)
     df_fn.to_csv(Path(evaluation_dir, "false-negative.csv"), index=False)
 
 
