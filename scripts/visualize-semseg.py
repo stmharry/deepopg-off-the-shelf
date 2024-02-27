7ef0f29009662bec42335bd7d6478d881f262066
Harry Hsu <h@stmharry.io>
2024-02-27 08:14:06 +0000

    feat: add probablistic map for semseg visualization

--- scripts/visualize-semseg.py
+++ scripts/visualize-semseg.py
@@ -1,6 +1,7 @@
 import contextlib
 from pathlib import Path
 
+import matplotlib.cm as cm
 import numpy as np
 from absl import app, flags, logging
 from pydantic import parse_obj_as
@@ -35,42 +36,94 @@ flags.DEFINE_integer("num_workers", 0, "Number of workers.")
 FLAGS = flags.FLAGS
 
 
-def visualize_data(
-    data: SemanticSegmentationData,
-    prediction: SemanticSegmentationPrediction,
-    metadata: Metadata,
-    visualize_dir: Path,
+def _visualize_binarized_semseg(
+    detectron2_instances: Instances,
+    visualizer: Visualizer,
+    output_image_path: Path,
 ) -> None:
-    output_image_path: Path = Path(visualize_dir, f"{prediction.file_name.stem}.png")
-    if not FLAGS.force and output_image_path.exists():
-        logging.info(f"Skipping {output_image_path!s}.")
-        return
-
-    image_rgb: np.ndarray = read_image(prediction.file_name)
-
-    visualizer = Visualizer(image_rgb, metadata=metadata)
-    detectron2_instances: Instances = prediction.to_detectron2_instances(
-        height=data.height, width=data.width
-    )
+    height: int = visualizer.output.height
+    width: int = visualizer.output.width
 
     semseg_mask: np.ndarray
     if len(detectron2_instances.pred_masks) == 0:
-        semseg_mask = np.zeros((data.height, data.width), dtype=np.uint8)
+        semseg_mask = np.zeros((height, width), dtype=np.uint8)
 
     else:
         pred_masks: np.ndarray = np.r_[
-            "0, 3",
-            np.zeros((data.height, data.width), dtype=np.uint8),
+            "0,3",
+            np.zeros((height, width), dtype=np.uint8),
             detectron2_instances.pred_masks,
         ]
         pred_classes: np.ndarray = np.r_[0, detectron2_instances.pred_classes]
 
         semseg_mask = pred_classes[np.argmax(pred_masks, axis=0)]
 
+    logging.info(f"Saving to {output_image_path!s}.")
     image_vis: VisImage = visualizer.draw_sem_seg(semseg_mask, alpha=0.5)
+    image_vis.save(output_image_path)
+
+
+def _visualize_heatmap_semseg(
+    npz_path: Path,
+    visualizer: Visualizer,
+    output_image_path: Path,
+) -> None:
+    with np.load(npz_path) as npz:
+        prob_uint16: np.ndarray = npz["prob"]  # (C, H, W)
+
+    prob: np.ndarray = prob_uint16.astype(np.float32) / 65535.0
+
+    for num, (class_prob, stuff_class) in enumerate(
+        zip(prob, visualizer.metadata.stuff_classes)
+    ):
+        if num == 0:
+            continue
+
+        visualizer.draw_soft_mask(
+            class_prob,
+            # we don't use metadata's color as it might be too continuous
+            color=cm.hsv(((num - 1) / 8) % 1),  # type: ignore
+            text=stuff_class.split("_")[1],
+            alpha=0.75,
+        )
 
     logging.info(f"Saving to {output_image_path!s}.")
+    image_vis: VisImage = visualizer.output
     image_vis.save(output_image_path)
+
+
+def visualize_data(
+    data: SemanticSegmentationData,
+    prediction: SemanticSegmentationPrediction,
+    metadata: Metadata,
+    visualize_dir: Path,
+) -> None:
+    output_image_path: Path
+    image_rgb: np.ndarray = read_image(prediction.file_name)
+
+    output_image_path = Path(
+        visualize_dir, f"{prediction.file_name.stem}.binarized.jpg"
+    )
+    if (not FLAGS.force) and output_image_path.exists():
+        logging.info(f"Skipping {output_image_path!s}.")
+    else:
+        _visualize_binarized_semseg(
+            detectron2_instances=prediction.to_detectron2_instances(
+                height=data.height, width=data.width
+            ),
+            visualizer=Visualizer(image_rgb, metadata=metadata),
+            output_image_path=output_image_path,
+        )
+
+    output_image_path = Path(visualize_dir, f"{prediction.file_name.stem}.heatmap.jpg")
+    if (not FLAGS.force) and output_image_path.exists():
+        logging.info(f"Skipping {output_image_path!s}.")
+    else:
+        _visualize_heatmap_semseg(
+            npz_path=Path(FLAGS.result_dir, "inference", f"{data.file_name.stem}.npz"),
+            visualizer=Visualizer(image_rgb, metadata=metadata),
+            output_image_path=output_image_path,
+        )
 
 
 def main(_):
